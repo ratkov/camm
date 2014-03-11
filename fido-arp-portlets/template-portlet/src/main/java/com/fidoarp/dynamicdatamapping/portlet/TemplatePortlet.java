67e8fe1266250c246a6926a5210bf89eea36dfdb
Kateryna Kliushnyk <k.kliushnyk@aimprosoft.com>
2014-03-11 19:59:19 +0200

    templates

--- fido-arp-portlets/template-portlet/src/main/java/com/fidoarp/dynamicdatamapping/portlet/TemplatePortlet.java
+++ fido-arp-portlets/template-portlet/src/main/java/com/fidoarp/dynamicdatamapping/portlet/TemplatePortlet.java
@@ -2,5 +2,6 @@ package com.fidoarp.dynamicdatamapping.portlet;
 
 import com.liferay.util.bridges.mvc.MVCPortlet;
 
-public class TemplatePortlet extends MVCPortlet {
+public class TemplatePortlet extends MVCPortlet{
+
 }
--- fido-arp-portlets/user-portlet/src/main/java/com/fidoarp/fidoarpusers/portlet/UserPortlet.java
+++
@@ -1,6 +0,0 @@
-package com.fidoarp.fidoarpusers.portlet;
-
-import com.liferay.util.bridges.mvc.MVCPortlet;
-
-public class UserPortlet extends MVCPortlet {
-}
